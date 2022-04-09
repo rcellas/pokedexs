5f70d2429697c4c51f511ee6ca4d6fa184dc9be6
Rocío Cejudo <rocio.cejudo.soronellas@gmail.com>
2022-04-09 19:20:42 +0200

    localstorage favorite pokemon

--- src/App.js
+++ src/App.js
@@ -4,59 +4,84 @@ import { getPokemons, getPokemonsData } from "./api";
 import Navbar from "./components/Navbar/Navbar";
 import SearchBar from "./components/SearchBar/SearchBar";
 import Pokedex from "./components/Pokedex/Pokedex";
-import {FavoriteProvider} from "./context/favoritePokemon";
+import { FavoriteProvider } from "./context/favoritePokemon";
 
 function App() {
   const [pokemons, setPokemon] = useState([]);
   const [page, setPage] = useState(0);
   const [total, setTotal] = useState();
   const [loading, setLoading] = useState(true);
-  const [favorite,setFavorite] = useState([]);
+  const [favorite, setFavorite] = useState([]);
+
+  const localStorageKey = "favorite_pokemons";
 
   const fetchPokemon = async () => {
-    try { 
-      setLoading(true)
+    try {
+      setLoading(true);
       const data = await getPokemons(25, 25 * page);
       const promises = data.results.map(async (pokemon) => {
         return await getPokemonsData(pokemon.url);
       });
       const results = await Promise.all(promises);
       setPokemon(results);
-      setTotal(Math.ceil(data.count / 25))
-      setLoading(false)
+      setTotal(Math.ceil(data.count / 25));
+      setLoading(false);
     } catch (err) {
       console.error(err);
     }
   };
+
+  // Mantein the favorites pokemons in localStorage
+  const loadFavoritePokemons = () => {
+    const pokemons =
+      JSON.parse(window.localStorage.getItem(localStorageKey)) || [];
+    setFavorite(pokemons);
+  };
+
+  useEffect(() => {
+    loadFavoritePokemons();
+  }, []);
 
   useEffect(() => {
     fetchPokemon();
   }, [page]);
 
   const updateFavoritePokemons = (name) => {
-    const updated =[...favorite];
+    const updated = [...favorite];
     const isFavorite = favorite.indexOf(name);
-    if(isFavorite >=0){
-      updated.splice(isFavorite,1);
-    } else{
+    if (isFavorite >= 0) {
+      updated.splice(isFavorite, 1);
+    } else {
       updated.push(name);
     }
     setFavorite(updated);
-  }
+    window.localStorage.setItem(localStorageKey, JSON.stringify(updated));
+  };
 
   return (
-    <FavoriteProvider value={{favPokemon:favorite, updateFavoritePokemons:updateFavoritePokemons }}>
-    <div>
-      <Navbar />
-      <div className="container mx-auto p-6">
-        <SearchBar />
-        {loading ? (
-          <div>Esperando respuesta ...</div>
-        ) : (
-          <Pokedex loading={loading} pokemons={pokemons} total={total} page={page} setPage={setPage} />
-        )}
+    <FavoriteProvider
+      value={{
+        favPokemon: favorite,
+        updateFavoritePokemons: updateFavoritePokemons,
+      }}
+    >
+      <div>
+        <Navbar />
+        <div className="container mx-auto p-6">
+          <SearchBar />
+          {loading ? (
+            <div>Esperando respuesta ...</div>
+          ) : (
+            <Pokedex
+              loading={loading}
+              pokemons={pokemons}
+              total={total}
+              page={page}
+              setPage={setPage}
+            />
+          )}
+        </div>
       </div>
-    </div>
     </FavoriteProvider>
   );
 }
