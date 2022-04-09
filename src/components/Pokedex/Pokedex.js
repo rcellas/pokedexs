c8da36c6f6c19d0a548fe2626092c234d47b3c2c
Rocío Cejudo <rocio.cejudo.soronellas@gmail.com>
2022-04-09 18:12:19 +0200

    pokedex cards

--- src/components/Pokedex/Pokedex.js
+++ src/components/Pokedex/Pokedex.js
@@ -1,23 +1,40 @@
 import React from "react";
-import PokemonFile from '../PokemonFile/PokemonFile';
+import Pagination from "../Pagination/Pagination";
+import PokemonFile from "../PokemonFile/PokemonFile";
 import "./Pokedex.scss";
 
 const Pokedex = (props) => {
-  console.log(props);
-  const { pokemons } = props;
+  const { pokemons, page, setPage, total, loading } = props;
+
+  const lastPages = () => {
+    const nextPage = Math.max(page - 1, 0);
+    setPage(nextPage);
+  };
+  const nextPages = () => {
+    const nextPage = Math.min(page + 1, total);
+    setPage(nextPage);
+  };
+
   return (
     <div className="pokedex">
       <header>
         <h1>Pokedex</h1>
-        <div>Paginación</div>
+        <Pagination
+          page={page + 1}
+          totalPages={total}
+          onLeftClick={lastPages}
+          onRightClick={nextPages}
+        />
       </header>
-      <div className="pokedex-grid">
-        {pokemons.map((pokemon,idx)=>{
-            return(
-                <PokemonFile pokemon={pokemon} key={pokemon.name}/>
-            )
-        })}
-      </div>
+      {loading ? (
+        <div>Cargando Pokemons...</div>
+      ) : (
+        <div className="pokedex-grid">
+          {pokemons.map((pokemon, idx) => {
+            return <PokemonFile pokemon={pokemon} key={pokemon.name} />;
+          })}
+        </div>
+      )}
     </div>
   );
 };
--- src/components/PokemonFile/PokemonFile.js
+++ src/components/PokemonFile/PokemonFile.js
@@ -1,31 +1,42 @@
-import React from 'react';
-import './PokemonFile.scss'
+import React from "react";
+import "./PokemonFile.scss";
 
-const Pokemon =(props)=>{
-    const {pokemon}=props;
-    console.log('da',pokemon)
-    return(
-        <div className='pokemonCard'>
-          <div className='pokemonImg'>
-            <img src={pokemon.sprites.front_default} alt={pokemon.name}/>
-          </div>
-          <div>
-              <div>
-                  <h3>{pokemon.name}</h3>
-                  <p>#{pokemon.id}</p>
-              </div>
-              <div>
-                  <div>
-                     {/* {pokemon.types.map((type,idx)=>{
-                         return(
-                             <div key={idx}>{type.name}</div>
-                         )
-                     })}  */}
-                  </div>
-              </div>
-            </div>  
+const Pokemon = (props) => {
+  const { pokemon } = props;
+
+  const redHeart = "❤️️";
+  const blackHeart = "❤";
+  return (
+    <div className="pokemonCard">
+      <div className="conteinerImg">
+        <img
+          src={pokemon.sprites.front_default}
+          alt={pokemon.name}
+          className="pokemonImg"
+        />
+      </div>
+      <div className="cardBody">
+        <div className="cardTop">
+          <h3>{pokemon.name}</h3>
+          <p>#{pokemon.id}</p>
         </div>
-    )
-}
+        <div className="cardFooter">
+          <div className="cardType">
+            {pokemon.types.map((type, idx) => {
+              return (
+                <div key={idx} className="cardTypeText">
+                  {type.type.name}
+                </div>
+              );
+            })}
+          </div>
+          <button className="cardFavorite">
+            {redHeart}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Pokemon;
